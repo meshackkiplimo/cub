c652ccc523dcb7364ae3c1c168a13acb1bea767e
kimaiyo <meshackkimaiyo5@gmail.com>
2025-05-28 09:47:58 +0300

    Refactor index.ts to implement Express server and remove customer-related database queries

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,24 @@
-import { eq } from "drizzle-orm";
-import db from "./drizzle/db";
-import { CustomerTable } from "./drizzle/schema";
+import express from "express";
+import dotenv from "dotenv";
 
-const getAllCustomers = async () => {
-    return await db.query.CustomerTable.findMany()
-}
-const getById = async (customer_id: number) => {
-    return await db.query.CustomerTable.findFirst({
-        where: eq(CustomerTable.customer_id, customer_id) 
-    })
+dotenv.config();
 
-}
 
-const getCustomerWithReservations = async (customer_id: number) => {
-    return await db.query.CustomerTable.findFirst({
-        where: eq(CustomerTable.customer_id, customer_id),
-        with: {
-            reservations: true // Assuming you have a relation defined in your schema
-        }
-    });
-}
+const app = express()
+const port = process.env.PORT
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello ')
+})
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
 
 
 
-async function main() {
-    const customers = await getAllCustomers();
-
-    
-    const customer = await getById(2);
-    if(!customer) {
-        
-        return;
-    }
-
-    const reserved = await getCustomerWithReservations(customer.customer_id);
-    if (!reserved) {
-        console.log("No reservations found for this customer.");
-        return;
-    }
-    console.log("Customer Details:", customer);
-    console.log("Reservations:", reserved.reservations);
-    
-}
-
-main().catch((error) => {
-    console.error("Error fetching customers:", error);
-    process.exit(1); // Exit with an error code
-});
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`)
+})
