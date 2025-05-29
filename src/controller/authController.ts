b188f3018af4428b35e5c24e4a53b1bb83c25777
kimaiyo <meshackkimaiyo5@gmail.com>
2025-05-29 22:22:26 +0300

    feat: add JWT authentication to user login process

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { createAuthService, loginAuthService } from '../services/authService';
 import { TIUserWithCustomer } from '../types';
+import jwt from 'jsonwebtoken';
 
 export const creatUserController = async (req: Request, res: Response) => {
     try {
@@ -39,7 +40,40 @@ export const loginUserController = async (req: Request, res: Response) => {
             res.status(401).json({ message: "Invalid credentials" });
             return;
         }
-        res.status(200).json({ message: "Login successful", user: existingUser });
+         const payload ={
+        sub: existingUser.user_id,
+        user_id: existingUser.user_id,
+        first_name: existingUser.first_name,
+        last_name: existingUser.last_name,
+        role:existingUser.role,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60), // Token expires in 1 hour
+        
+    }
+        // Here you would typically generate a JWT token using a library like jsonwebtoken
+        const secret =process.env.JWT as string;
+        if (!secret) {
+
+            throw new Error("JWT secret is not defined");
+        }
+        const token = jwt.sign(payload, secret);
+        console.log("Token generated:", token);
+        return res.status(200).json(
+            { 
+                message: "Login successful", 
+                
+                user: {
+                    id: existingUser.user_id,
+                    first_name: existingUser.first_name,
+                    last_name: existingUser.last_name,
+                    email: existingUser.email
+                }, 
+                token 
+
+
+
+            });
+
+        
     } catch (error) {
         console.error("Error in loginUserController:", error);
         res.status(500).json({ message: "Internal server error" });
