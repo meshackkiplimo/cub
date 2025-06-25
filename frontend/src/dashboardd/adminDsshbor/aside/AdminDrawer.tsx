a3197fb470da5ff8ddf9a4b9889aaaf166c1f611
kimaiyo <meshackkimaiyo5@gmail.com>
2025-06-25 22:09:22 +0300

    style: Update AdminDrawer link hover background color to green

--- frontend/src/dashboardd/adminDsshbor/aside/AdminDrawer.tsx
+++ frontend/src/dashboardd/adminDsshbor/aside/AdminDrawer.tsx
@@ -14,7 +14,7 @@ const AdminDrawer = () => {
                         <li key={item.id}>
                             <Link
                                 to={item.link}
-                                className="flex space-x-3 border-b-2 border-transparent hover:border-blue-400 text-white hover:bg-gray-700 p-4"
+                                className="flex space-x-3 border-b-2 border-transparent hover:border-blue-400 text-white hover:bg-green-700 p-4"
                             >
                                 <item.icon size={30} />
                                 <span className="text-xl text-gray-100 mb-2">{item.name}</span>
